d1c964316128f7434f8f09bdf7eb447aa7c4f3c2
Hannes Wallnöfer <hannesw@gmail.com>
2010-08-06 14:23:07 +0200

    Fix indentation

--- actions.js
+++ actions.js
@@ -10,41 +10,41 @@ var welcomePages = ["index.html","index.md"];
 
 exports.index = function (req,path)
 {
-      var uriPath = fileutils.resolveUri(req.rootPath, path);
-      var absolutePath=fs.join(root,path);
-	var isDirectory=fs.isDirectory(absolutePath);
-	
-	if(isDirectory)
-      {
-		for each(var name in welcomePages)
-		{
-			if(fs.isFile(fs.join(absolutePath,name)))
-			{
-				return serveFile(fs.join(absolutePath,name));	
-			}		
-		}
-      }
-	
-	if(fs.isFile(absolutePath))
-	{
-		return serveFile(absolutePath);
-	}
+    var uriPath = fileutils.resolveUri(req.rootPath, path);
+    var absolutePath=fs.join(root,path);
+    var isDirectory=fs.isDirectory(absolutePath);
 
-      if(isDirectory)
-      {
-          if (!strings.endsWith(uriPath, "/")) {
-              throw {redirect: uriPath + "/"};
-          }
-          return listFiles(absolutePath, uriPath);
-      }
-      throw {notfound:true};
+    if(isDirectory)
+    {
+        for each(var name in welcomePages)
+        {
+            if(fs.isFile(fs.join(absolutePath,name)))
+            {
+                return serveFile(fs.join(absolutePath,name));
+            }
+        }
+    }
+
+    if(fs.isFile(absolutePath))
+    {
+        return serveFile(absolutePath);
+    }
+
+    if(isDirectory)
+    {
+        if (!strings.endsWith(uriPath, "/")) {
+            throw {redirect: uriPath + "/"};
+        }
+        return listFiles(absolutePath, uriPath);
+    }
+    throw {notfound:true};
 };
 
 function listFiles(absolutePath,uriPath)
 {
-   var files = fs.list(absolutePath).sort();
-   files = files.map(function(file)
-   {
+    var files = fs.list(absolutePath).sort();
+    files = files.map(function(file)
+    {
         var filePath = fs.join(absolutePath,file)
         return {
             name:file,
@@ -52,21 +52,21 @@ function listFiles(absolutePath,uriPath)
             lastModified:fs.lastModified(filePath),
             path:fileutils.resolveUri(uriPath,file)
         };
-   });
+    });
 
-   return skinResponse('./skins/list.html', {
-      files: files,
-   });
+    return skinResponse('./skins/list.html', {
+        files: files,
+    });
 }
 
 function serveFile(absolutePath)
 {
     if(fs.extension(absolutePath)==".md")
     {
-         var html = md.Markdown().process(fs.read(absolutePath));
-            return skinResponse('./skins/page.html', {
-	 content: html,
-	 });
+        var html = md.Markdown().process(fs.read(absolutePath));
+        return skinResponse('./skins/page.html', {
+            content: html,
+        });
     }
     return staticResponse(absolutePath);
 }
--- config.js
+++ config.js
@@ -1,25 +1,25 @@
 
-//root directory for published content - please adapt
+// root directory for public content - please adapt
 exports.root = ".";
 
 exports.urls = [
     ['/(.*)', require('./actions')],
-    ];
+];
 
 exports.middleware = [
-    require('ringo/middleware/gzip').middleware,        
+    require('ringo/middleware/gzip').middleware,
     require('ringo/middleware/etag').middleware,
     require('ringo/middleware/responselog').middleware,
     require('ringo/middleware/error').middleware,
     require('ringo/middleware/notfound').middleware
-    ];
+];
 
 exports.app = require('ringo/webapp').handleRequest;
 
 exports.macros = [
     require('ringo/skin/macros'),
     require('ringo/skin/filters'),
-    ];
+];
 
 exports.charset = 'UTF-8';
 exports.contentType = 'text/html';
